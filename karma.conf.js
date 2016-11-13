65a9a5b662521c7683e86e0cf9c6ec64fcefec7a
chrisminnick <chris@watzthis.com>
2016-11-13 09:43:12 -0800

    fixed karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -8,6 +8,7 @@ module.exports = function(config) {
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
+    plugins: ['karma-jasmine','karma-webpack','karma-chrome-launcher','karma-commonjs'],
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
