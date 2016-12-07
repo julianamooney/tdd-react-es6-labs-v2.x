fe15bccb6f247306d072bfaa8460bfa146caeb74
Chris Minnick <chris@minnick.com>
2016-12-07 14:11:19 -0800

    updated submitbuttonspec

--- src/components/__tests__/pollSubmitButtonSpec.js
+++ src/components/__tests__/pollSubmitButtonSpec.js
@@ -1,6 +1,11 @@
+/* eslint-disable */
+
+jest.dontMock('../PollSubmitButton');
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
+
 
 describe('Poll Submit Button', function() {
 
@@ -14,6 +19,26 @@ describe('Poll Submit Button', function() {
         var buttonText = ReactDOM.findDOMNode(pollsubmitbutton).textContent;
 
         expect(buttonText).toEqual('Go!');
+    });
+
+    it('calls handler function on click', function () {
+
+        var PollSubmitButton = require('../PollSubmitButton').default;
+
+        var handleClick = jest.genMockFunction();
+
+        var pollsubmitbutton = TestUtils
+            .renderIntoDocument(<PollSubmitButton question={0} handleClick={handleClick}/>);
+
+        var buttonInstance = ReactDOM.findDOMNode(pollsubmitbutton);
+
+        TestUtils.Simulate.click(buttonInstance);
+
+        expect(handleClick).toBeCalled();
+
+        var numberOfCallsMadeIntoMockFunction = handleClick.mock.calls.length;
+
+        expect(numberOfCallsMadeIntoMockFunction).toBe(1);
     });
 
 });
