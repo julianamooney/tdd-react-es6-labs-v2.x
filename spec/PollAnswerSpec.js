a990fe684557834c0d5d144189723e04fd48dd69
chrisminnick <chris@watzthis.com>
2017-05-23 13:00:53 -0700

    removed gulp

--- spec/PollAnswerSpec.js
+++ spec/PollAnswerSpec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import TestUtils from 'react-addons-test-utils';
+import TestUtils from 'react-dom/test-utils';
 import PollAnswer from '../src/components/PollAnswer.js';
 
 describe('Poll Answer', function() {
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,31 @@
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+
 module.exports = {
+    devtool: 'source-map',
     entry: './src/scripts/app.js',
     output: {
-        filename: 'app.js'
+        path: path.resolve(__dirname, 'dist'),
+        filename: './scripts/app.js'
     },
-    debug: true,
-    devtool:'source-map',
     module : {
         loaders: [ {
-            test : /.js$/,
+            test   : /.js$/,
             loader : 'babel-loader',
-            query : {
+            query: {
                 presets: ['es2015','react']
             }
-        }]
-    }/*,
+        }
+        ]
+    },
     plugins: [
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': '"production"'
-            }
+        new CleanWebpackPlugin('dist'),
+        new HTMLWebpackPlugin({
+            filename: 'index.html',
+            title: 'Welcome to my poll!',
+            template: 'src/index.html'
         })
-    ]*/
+    ]
 };
