37cdd212ad899b893c16ab534638ad3d362faf0d
chrisminnick <chris@watzthis.com>
2017-05-23 12:54:03 -0700

    fixed npm start task

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
             test   : /.js$/,
             loader : 'babel-loader',
             query: {
-                presets: ['es2015','react','react-hmre']
+                presets: ['es2015','react']
             }
         }
         ]
