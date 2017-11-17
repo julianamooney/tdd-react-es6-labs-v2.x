b43eecc9be3e287565d0f7b06547a1f2c23d7990
chrisminnick <chris@watzthis.com>
2017-11-16 16:31:16 -0800

    removing solutions directory

--- solutions/swimcalc-basic-ssr/src/scripts/app.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PageContainer from '../containers/PageContainer';
-
-
-ReactDOM.hydrate(<PageContainer />,
-    document.getElementById('app'));
--- solutions/swimcalc-basic/src/scripts/app.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PageContainer from '../containers/PageContainer';
-
-
-ReactDOM.render(<PageContainer />,
-    document.getElementById('app'));
