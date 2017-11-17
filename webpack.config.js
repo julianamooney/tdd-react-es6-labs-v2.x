abf5543bde7db90d6d035c35a9514833f0693fac
chrisminnick <chris@watzthis.com>
2017-11-16 16:44:22 -0800

    upgraded to 16

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,22 @@
+const path = require('path');
+const webpack = require('webpack');
+
 module.exports = {
     entry  : './src/scripts/app.js',
-    output : {
-        filename : 'app.js'
+    output: {
+        path: path.join(__dirname, 'build/'),
+        filename: 'index.js',
+        publicPath: '/build/'
     },
     module : {
         loaders: [ {
             test   : /.js$/,
             loader : 'babel-loader',
             query: {
-                presets: ['es2015','react']
+                presets: ['es2015','react','stage-2']
             }
         }
         ]
-    }/*,
-     plugins: [
-     new webpack.DefinePlugin({
-     'process.env': {
-     'NODE_ENV': '"production"'
-     }
-     })
-     ]*/
+    }
 
 };
