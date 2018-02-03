a55a7073bb0743b6ca4e29925d0b5db36159a7fc
chrisminnick <chris@watzthis.com>
2018-02-02 17:03:04 -0800

    added solutions

--- spec/sayHelloSpec.js
+++ spec/sayHelloSpec.js
@@ -12,4 +12,18 @@ describe('Greet', function() {
         var expected = 'Hello, Friend!';
         expect(actual).toEqual(expected);
     });
+
+    it('says good morning in the morning', function() {
+        var theDate = new Date('February 01, 2018 08:00:00');
+
+        var actual = sayHello.greet('World',theDate);
+        var expected = 'Good Morning, World';
+        expect(actual).toEqual(expected);
+
+    });
+    it('speaks German to Germans', function() {
+        var actual = sayHello.greet('Hans');
+        var expected = 'Guten Tag, Hans';
+        expect(actual).toEqual(expected);
+    });
 });
