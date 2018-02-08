ceb9dc87409afafdcaaeb5291d5e6ead064cf39b
Chris Minnick <chris@minnick.com>
2018-02-08 13:44:24 -0500

    converted a test to use enzyme

--- spec/PollSubmitButton.test.js
+++ spec/PollSubmitButton.test.js
@@ -8,13 +8,8 @@ describe('Poll Submit Button', function() {
 
 
     it('renders without a problem', function () {
-
-        var pollsubmitbutton = TestUtils
-            .renderIntoDocument(<PollSubmitButton />);
-
-        var buttonText = ReactDOM.findDOMNode(pollsubmitbutton).textContent;
-
-        expect(buttonText).toEqual('Go!');
+        var pollsubmitbutton = shallow(<PollSubmitButton />);
+        expect(pollsubmitbutton.text()).toEqual('Go!');
     });
 
 
