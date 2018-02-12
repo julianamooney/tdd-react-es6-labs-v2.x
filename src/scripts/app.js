ab2bcaea42f4505859d5fa155f137407d20fd117
chrisminnick <chris@watzthis.com>
2018-02-12 11:03:04 -0800

    added event listener

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,6 @@
 var sayHello = require('./sayHello.js');
 
-document.getElementById('welcome-message').innerHTML = sayHello.greet('Chris');
+window.addEventListener('load',function(){
+    document.getElementById('welcome-message').innerHTML = sayHello.greet('Chris');
+});
 
