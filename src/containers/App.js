8a9499651049b65e2d13293e70b3c825e4ed8583
chrisminnick <chris@watzthis.com>
2018-03-06 15:30:46 -0800

    updated to react-router-dom 4

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import {Link} from 'react-router';
+import {Link, Route} from 'react-router-dom';
+import PollContainer from './PollContainer';
+import AboutUs from '../components/AboutUs';
 
 class App extends React.Component {
     render(){
         return(
             <div>
                 <ul className = "nav nav-pills">
-                    <li role="presentation" class="active"><Link to="/">Home</Link></li>
-                    <li role="presentation" class="active"><Link to="poll">Poll</Link></li>
-                    <li role="presentation" class="active"><Link to="about">About Us</Link></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/poll">Poll</Link></li>
+                    <li><Link to="/about">About Us</Link></li>
                 </ul>
                 <div>
-                    {this.props.children}
+                    <Route path="/poll" component={PollContainer}/>
+                    <Route path="/about" component={AboutUs}/>
                 </div>
             </div>
         );
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, Route, hashHistory} from 'react-router';
+import {BrowserRouter} from 'react-router-dom';
 
 import App from '../containers/App';
-import PollContainer from '../containers/PollContainer';
-import AboutUs from '../components/AboutUs';
 
 
 ReactDOM.render((
-    <Router history={hashHistory}>
-        <Route path="/" component={App}>
-            <Route path="poll" component={PollContainer} />
-            <Route path="about" component={AboutUs} />
-        </Route>
-    </Router>),
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>),
     document.getElementById('app')
 );
