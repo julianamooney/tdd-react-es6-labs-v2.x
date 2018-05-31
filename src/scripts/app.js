8ec3d49e1b6d9e6670d676e316f4c9593fd476a7
chrisminnick <chris@watzthis.com>
2018-05-31 10:18:14 -0700

    updated to match lab12

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,5 @@
 var sayHello = require('./sayHello.js');
-
-document.getElementById('welcome-message').innerHTML = sayHello.greet('Chris');
-
+window.addEventListener('load', function(){
+    document.getElementById('welcome-message')
+        .innerHTML = sayHello.greet('Your name');
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,23 @@
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const path = require('path');
 
-
 module.exports = {
+    mode: 'production',
     devtool: 'source-map',
     entry: './src/scripts/app.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: './scripts/app.js'
+        filename: 'scripts/app.js'
     },
-    plugins: [
+
+    plugins: [ 
         new CleanWebpackPlugin('dist'),
-        new HTMLWebpackPlugin({
-            filename: 'index.html',
-            title: 'Welcome to my page!',
-            mainDiv: 'welcome-message',
-            template: 'src/index.html'
+        new HTMLWebpackPlugin({ 
+            filename: 'index.html', 
+            title: 'Welcome to my page!', 
+            mainDiv: 'welcome-message', 
+            template: 'src/index.html' 
         })
     ]
 };
