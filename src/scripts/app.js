7c73da893875e133419b45beb22e9c448662e3b4
chrisminnick <chris@watzthis.com>
2018-05-31 10:20:30 -0700

    updated for webpack 4

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,5 @@
 const sayHello = require('./sayHello.js');
-
-window.addEventListener('load',()=>{
-    document.getElementById('welcome-message').innerHTML = sayHello.greet('Chris');
+window.addEventListener('load', ()=>{
+    document.getElementById('welcome-message')
+        .innerHTML = sayHello.greet('Your name');
 });
-
